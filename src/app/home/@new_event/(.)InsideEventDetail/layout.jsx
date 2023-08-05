251a3e41d024fd58f4279bec5a136db78c3356e5
umeshSinghVerma <usv0308@gmail.com>
2023-08-05 23:25:30 +0530

    Event fixes

--- src/app/home/@new_event/(.)InsideEventDetail/layout.jsx
+++ src/app/home/@new_event/(.)InsideEventDetail/layout.jsx
@@ -1,15 +1,20 @@
 'use client'
 import Popup from '@/components/Popup'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { useParams, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function InsideEventLayout(props) {
     const router = useRouter();
+    const params = useParams();
+    const [currentComp,setCurrentComp]=useState(params.comp);
+    useEffect(()=>{
+        setCurrentComp(params.comp);
+    },[params.comp])
     return (
         <div>
             <Popup>
-                <div className='w-full h-[80vh] overflow-hidden rounded-lg bg-white text-black relative p-8'>
+                <div className='w-full max-h-[80vh] overflow-hidden rounded-lg bg-white text-black relative p-8'>
                     <button
                         onClick={() => router.back()}
                         className='text-black absolute top-4 left-5'
@@ -22,11 +27,11 @@ export default function InsideEventLayout(props) {
                                 הוספת אירוע חדש
                             </h1>
                             <div className='flex'>
-                                <Link replace href='/home/InsideEventDetail/1' className='border-black border-l-0 border flex p-2 gap-2'>
+                                <Link replace href='/home/InsideEventDetail/1' className={`border-black  border flex p-2 gap-2 ${currentComp==1 && 'opacity-50'}`}>
                                     <p className='border-black text-sm border-solid border rounded-full w-6 flex justify-center items-center'>1</p>
                                     <p>פרטי האירוע</p>
                                 </Link>
-                                <Link replace href='/home/InsideEventDetail/2' className='border-black border flex p-2 gap-2'>
+                                <Link replace href='/home/InsideEventDetail/2' className={`border-black border-r-0 border flex p-2 gap-2 ${currentComp==2 && 'opacity-50'}`}>
                                     <p className='border-black text-sm border-solid border rounded-full w-6 flex justify-center items-center'>2</p>
                                     <p>פרטי החוגגים</p>
                                 </Link>
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -15,14 +15,14 @@ const page = () => {
       {/* <Sidebar /> */}
       {/* <DataStats /> */}
       {/* <Connections /> */}
-      {/* <div className='flex flex-col'>
+      <div className='flex flex-col'>
         <Link href='/home/InsideEventDetail/1'>InsiderEventDetail1</Link>
         <Link href='/home/e1'>e1</Link>
         <Link href='/home/e2'>e2</Link>
         <Link href='/home/e11'>e11</Link>
         <Link href='/home/postModle'>PostModle</Link>
         <Link href='/contact'>Contact</Link>
-      </div> */}
+      </div>
       {/* <Post /> */}
       {/* <ImageComp /> */}
       {/* <div id='sidenav' className='absolute left-0 z-[10000]'>
